d851c6a481f31839e9d816731d9d9f9aa6c98730
othman <web.othman01@gmail.com>
2023-07-14 15:15:49 -0700

    add 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints whether a randomly generated number is positive, negative, or zero
+ * Description:  generated number is positive, negative, or zero
  *
  * Return: Always 0 (Success)
  */
@@ -25,6 +25,7 @@ int main(void)
 
 	return (0);
 }
+
 
     
    
