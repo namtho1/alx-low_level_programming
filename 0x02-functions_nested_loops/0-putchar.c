6a7e3cb67693594af31b6434bb9330dbec0c07bb
othman <web.othman01@gmail.com>
2023-07-18 15:25:13 -0700

    Add 0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
-/*
+/**
  * 0-putchar.c
- * Description main:outputs the string "_putchar" followed by a newline character 
+ * Description main:outputs the string "_putchar" followed by a newline. 
  */
 #include <stdio.h>
 #include "main.h"
@@ -16,4 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return 0;}
+ return (0);
+}
