3d84d24a0177c8359e2b8c31ba7efb939c702e51
othman <web.othman01@gmail.com>
2023-07-18 15:15:29 -0700

    Add 0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
-/* 0-putchar.c
- *
- * Description: Entry point of the program that demonstrates the usage of the _putchar function.
- * The program outputs the string "_putchar" followed by a newline character to the standard output.
+/*
+ * 0-putchar.c
+ * Description:outputs the string "_putchar" followed by a newline character 
  */
 #include <stdio.h>
 #include "main.h"
 
 int main(void)
 {
- _putchar('_');
- _putchar('p');
- _putchar('u');
- _putchar('t');
- _putchar('c');
- _putchar('h');
- _putchar('a');
- _putchar('r');
- _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
     return 0;
 }
