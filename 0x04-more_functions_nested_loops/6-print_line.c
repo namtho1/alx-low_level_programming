953de68b40d9b9da2591660690d40965aac9a2df
othman <web.othman01@gmail.com>
2023-07-20 19:28:37 -0700

    add 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,17 +6,17 @@
  */
 void print_line(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    while (n > 0)
-    {
-        _putchar('_');
-        n--;
-    }
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
