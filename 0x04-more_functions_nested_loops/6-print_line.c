70d3d5eff4cf51a1ff74825c8799661e2fd31ef6
othman <web.othman01@gmail.com>
2023-07-20 19:21:57 -0700

    add 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a straight line of underscores in the terminal.
+ * @n: The number of times the character '_' should be printed.
+ */
+void print_line(int n)
+{
+    if (n <= 0)
+    {
+        _putchar('\n');
+        return;
+    }
+
+    while (n > 0)
+    {
+        _putchar('_');
+        n--;
+    }
+
+    _putchar('\n');
+}
