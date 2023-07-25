329af2f4151ad300969204ed38fc5c562c018d1d
othman <web.othman01@gmail.com>
2023-07-24 19:53:42 -0700

    add 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,15 +9,15 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
 
-    dest[i] = '\0'; /* Add the null terminator at the end of the copied string */
+	dest[i] = '\0'; /* Add the null terminator at the end of the copied string */
 
-    return dest;
+	return (dest);
 }
