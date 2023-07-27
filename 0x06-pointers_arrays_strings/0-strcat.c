ff0f0880404d9591a275d7aaed9317a74824f61b
othman <web.othman01@gmail.com>
2023-07-27 09:49:26 -0700

    add 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,24 +7,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0, j = 0;
+	int i = 0, j = 0;
 
-    /* Find the end of the destination string */
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
+	/* Find the end of the destination string */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-    /* Concatenate the source string to the destination */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	/* Concatenate the source string to the destination */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    /* Add the null terminator to the end of the concatenated string */
-    dest[i] = '\0';
+	/* Add the null terminator to the end of the concatenated string */
+	dest[i] = '\0';
 
-    return dest;
+	return (dest);
 }
