f457368d4f9318ebe99c7c810b4ce0540ec27e43
othman <web.othman01@gmail.com>
2023-07-27 17:33:51 -0700

    add 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,16 +6,16 @@
  */
 char *string_toupper(char *str)
 { 
-	int i = 0;
+    int i = 0;
 
-	while (str[i] != '\0')
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - ('a' - 'A');
-		}
-		i++;
-	}
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - ('a' - 'A');
+        }
+        i++;
+    }
 
-	return (str);
+    return str;
 }
