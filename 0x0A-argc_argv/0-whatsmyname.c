15346696cc2cccb3f95cac0c21c95aef071472a1
othman <web.othman01@gmail.com>
2023-08-06 16:48:50 -0700

    add 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,14 @@
 
 /**
  * main - prints program name, followed by a new line
- * @argc:  int
+ * @argc: int
  * @argv: list
  *
  * Return: 0
  */
 int main(int argc, char const *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
