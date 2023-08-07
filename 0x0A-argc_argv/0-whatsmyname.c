935bc993768be98208acd29a004ab8fdb7ff0e4d
othman <web.othman01@gmail.com>
2023-08-07 09:44:38 -0700

    add 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,14 @@
 
 /**
  * main - prints program name, followed by a new line
- * @argc: int
- * @argv: list
+ * @argc: number of command-line arguments
+ * @argv: array of command-line arguments
  *
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
+	(void)argc; /* Suppress unused parameter warning */
 	printf("%s\n", argv[0]);
 	return (0);
 }
