73115117d6f5b7caae81f1a2c0ee4a3a3a60c1e2
othman <web.othman01@gmail.com>
2023-08-17 09:28:28 -0700

    add 0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Calculate sum of variable integers.
+ * @n: Number of integers.
+ * @...: Integers to sum.
+ * Return: Sum or 0 if n is 0.
+ */
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+if (n == 0)
+return (0);
+
+unsigned int i;
+int sum = 0;
+va_list args;
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+sum += va_arg(args, int);
+
+
+va_end(args);
+return (sum);
+}
+
