c37bfcfc6a2515a3d20d9ba05b664bf447025321
othman <web.othman01@gmail.com>
2023-08-26 15:44:27 -0700

    add 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,7 +20,7 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len])
 		len++;
 
-	new = malloc(sizeof(list_t));
+	newnode = malloc(sizeof(list_t));
 	if (!newnode)
 		return (NULL);
 
