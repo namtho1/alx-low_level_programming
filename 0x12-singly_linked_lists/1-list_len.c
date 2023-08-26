61aedabd9e8c5bbf2496ee27f39ac8d7188f060e
othman <web.othman01@gmail.com>
2023-08-26 07:32:33 -0700

    add 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - Returns the number of elements in a linked list_t list.
+ * @h: Pointer to the head of the list.
+ * Return: The number of elements in the list.
+ */
+int list_len(const list_t *h)
+{
+    int count = 0;
+
+    while (h != NULL)
+    {
+        count++;
+        h = h->next; 
+    }
+
+    return (count);
+}
+
