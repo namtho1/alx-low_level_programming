178b5aecbf7ec1987a79bf63f11246de916b6cad
othmanettahiri <web.othman01@gmail.com>
2023-09-21 07:11:38 -0700

    add main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h> 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdarg.h>
+
+#define BUFSIZE 1024
+
+int _putchar(char c);
+int _strlen(char *s);
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *file_from, const char *file_to);
 
 #endif /* MAIN_H */
